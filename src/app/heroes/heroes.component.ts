4f23e34965404d03cbf2d01a7dd862fd1c5c3375
gerald <oforige@gmail.com>
2021-12-01 14:27:11 +0000

    angular

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 
 
@@ -8,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstrom';
-  
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11,name: 'Storm'},
+    { id: 12,name: 'Wolverine'},
+    { id: 13,name: 'Quick Silver'},
+    { id: 14,name: 'Rogue'},
+    { id: 15,name: 'Magneto'},
+    { id: 16,name: 'Cyclops' },
+    { id: 17,name: 'Beast'},
+    { id: 18,name: 'Jean'},
+    { id: 19,name: 'Professor X'},
+    { id: 20,name: 'Ice man'}
+]
