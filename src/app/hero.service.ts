96ac00d7d1743551d4514b28ead49897911fa2d3
gerald <oforige@gmail.com>
2021-12-12 16:20:55 +0000

    able to add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,11 +21,31 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const  url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero> (`getHero id=${id}`))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any>{
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>(`updateHero`))
+    );
 
   }
+
+  addHero(hero: Hero): Observable<Hero>{
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero)=> this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-type': 'application/json'})
+  };
 
   private log(message: string) {
   this.messageService.add(`HeroService: ${message}`);
