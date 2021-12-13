4b21c1b9e4f109a994ad1440ec7c86d06249009a
gerald <oforige@gmail.com>
2021-12-13 11:39:10 +0000

    add and delete hero buttons

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -43,6 +43,15 @@ export class HeroService {
     );
   }
 
+  deleteHero(id: number): Observable<Hero>{
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    )
+  }
+
   httpOptions = {
     headers: new HttpHeaders({'Content-type': 'application/json'})
   };
